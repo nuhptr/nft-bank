44d2f79eebe22cc7ea35f7e70477d528247d4c16
nuhptr <nugrahaadi733@gmail.com>
2023-09-25 12:40:42 +0700

    change

--- src/components/feedback-card.tsx
+++ src/components/feedback-card.tsx
@@ -1,5 +1,3 @@
-import { quotes } from '../assets'
-
 interface FeedbackCardProps {
   content: string
   name: string
@@ -10,7 +8,11 @@ interface FeedbackCardProps {
 export default function FeedbackCard({ content, name, title, img }: FeedbackCardProps) {
   return (
     <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-20 sm:mr-5 mr-0 my-5 feedback-card'>
-      <img src={quotes} alt='double_quotes' className='w-[42px] h-[27px] object-contain' />
+      <img
+        src={'../assets/quotes.svg'}
+        alt='double_quotes'
+        className='w-[42px] h-[27px] object-contain'
+      />
       <p className='font-poppins font-normal text-[18px] leading-[32px] text-white my-10'>
         {content}
       </p>
